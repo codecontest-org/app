0094bdba5f9fd7ac6b23cb2b43de64c5e2b414d9
Macuyler Dunn <macuyler@gmail.com>
2021-06-14 13:47:44 -0500

    Add requirement notes to ContestInterface

--- src/components/Interfaces/Contest.jsx
+++ src/components/Interfaces/Contest.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Container, Typography, makeStyles } from '@material-ui/core';
+
+/**
+ * -- Contest Interface --
+ * This is designed to be the hub for everything contestants need during their contest!
+ * It should have links to the following pages:
+ *  - Tutorials
+ *  - Games
+ *  - Teams
+ *  - Class Select
+ *  - Replit setting
+ *  - Submission
+ *  - Notes?
+ */
 
 const ContestInterface = () => {
-  return <h1>Hello World</h1>;
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <Typography variant="h2" align="center">
+        Code Contest Dashboard
+      </Typography>
+    </Container>
+  );
 };
+
+const useStyles = makeStyles({
+  container: {
+    paddingTop: 20
+  }
+});
 
 export default ContestInterface;
