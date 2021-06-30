e307f9f8551f677c81c7db3d9405e31216b74e11
Macuyler Dunn <macuyler@gmail.com>
2021-06-30 11:16:11 -0500

    Add basic invite modal layout

--- src/components/Contest/Team/InviteModal.jsx
+++ src/components/Contest/Team/InviteModal.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CircularProgress, Typography } from '@material-ui/core';
+import {
+  Button,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
 import Modal from '../../UI/Modal';
 import { useChildren } from '../../../hooks/children';
 
@@ -18,22 +27,45 @@ const defaultProps = {
 
 const InviteModal = ({ open, onClose, cls }) => {
   const [children, isLoading] = useChildren(cls?.children);
+  const classes = useStyles();
   return (
     <Modal open={open} onClose={onClose}>
       <Typography variant="h3">Invite Members</Typography>
       {isLoading ? (
         <CircularProgress />
       ) : (
-        children.map(child => (
-          <Typography variant="body1" key={child.id}>
-            {child.fName}
-          </Typography>
-        ))
+        <List classes={{ root: classes.listRoot }}>
+          {children.map(child => (
+            <ListItem key={child.id} divider>
+              <ListItemText>
+                {child.fName} {child.lName}
+              </ListItemText>
+              <ListItemSecondaryAction>
+                <Button variant="outlined" color="secondary">
+                  Invite
+                </Button>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
       )}
+      <Button onClick={onClose} className={classes.closeBtn}>
+        Close
+      </Button>
     </Modal>
   );
 };
 InviteModal.propTypes = propTypes;
 InviteModal.defaultProps = defaultProps;
+
+const useStyles = makeStyles({
+  listRoot: {
+    width: '100%'
+  },
+  closeBtn: {
+    marginTop: 15,
+    padding: '6px 32px'
+  }
+});
 
 export default InviteModal;
