b86ed22958b39ad94d7503bad0de13c4e20972d1
Macuyler Dunn <macuyler@gmail.com>
2021-07-01 15:59:12 -0500

    Add useLiveTeamInvites hook

--- src/hooks/teams.js
+++ src/hooks/teams.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { onSnapshotDataEffectBase } from '../utils/effectBases';
+import { toData } from '../utils/helpers';
+import { db } from '../utils/firebase';
 
 /* ==================
  * === Team Hooks ===
@@ -14,8 +16,20 @@ export const useLiveTeamData = ref => {
   return team;
 };
 
+/**
+ * Subscribe to the data of all invites for a team.
+ */
+export const useLiveTeamInvites = id => {
+  const [invites, setInvites] = useState([]);
+  const refs = useMemo(() => db.collection('contestTeamInvites').where('teamId', '==', id), [id]);
+  const handleError = () => setInvites([]);
+  useEffect(liveInvitesDataEffect(refs, setInvites, handleError), [refs]);
+  return invites;
+};
+
 /* ===============================
  * === Custom Reusable Effects ===
  * =============================== */
 
 const liveTeamDataEffect = onSnapshotDataEffectBase(true);
+const liveInvitesDataEffect = onSnapshotDataEffectBase(true, snap => snap.docs.map(toData));
