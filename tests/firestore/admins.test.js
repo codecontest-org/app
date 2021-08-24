4c444f347eb84a3b38566316c09bbba11363c24d
Macuyler Dunn <macuyler@gmail.com>
2021-08-24 12:37:22 -0500

    Add create permission tests for the admins collection

--- tests/firestore/admins.test.js
+++ tests/firestore/admins.test.js
@@ -4,17 +4,15 @@ const { attempt, results } = require('../utils/helpers');
 const { db, auth } = useFirebase();
 
 // Local helpers.
-const doc = id => db.collection('admins').doc(id);
+const doc = id => db.collection('admins').doc(id || auth.id());
+const shouldFail = results(expect, false);
 
 test('All users can NOT read any admin documents.', async () => {
   expect.assertions(3);
 
   // Setup test document.
   await auth.admin();
-  const adminDoc = doc(auth.id());
-
-  // Setup results handler.
-  const shouldFail = results(expect, false);
+  const adminDoc = doc();
 
   /**
    * Test against admins, parents, and anonymous.
@@ -29,4 +27,24 @@ test('All users can NOT read any admin documents.', async () => {
 
   await auth.none();
   await attempt(adminDoc.get(), shouldFail);
+});
+
+test('All users can NOT create an admin document.', async () => {
+  expect.assertions(3);
+
+  const data = { secret: 'SUPER_SECRET' };
+
+  /**
+   * Test against admins, parents, and anonymous.
+   * Parents are considered non-admin users, so it
+   * would be redundant to test against teachers.
+   */
+  await auth.admin();
+  await attempt(doc().set(data), shouldFail);
+
+  await auth.parent();
+  await attempt(doc().set(data), shouldFail);
+
+  await auth.none();
+  await attempt(doc().set(data), shouldFail);
 });
